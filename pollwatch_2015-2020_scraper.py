8edc5eca5169b9e38dda4ad31a2edb2279f38f6c
Benjamin Pelc <bjpelcdev@outlook.com>
2016-03-17 12:42:35 +0000

    All fields parsing

--- pollwatch_2015-2020_scraper.py
+++ pollwatch_2015-2020_scraper.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-from datetime import datetime as dt
+import datetime as dt
 import urllib
 import re
 
@@ -25,17 +25,60 @@ def ge_poll_tables():
 def is_poll_row(row):
     return len(row.find_all("td")) == 11
 
+# month_string_to_number
+def month_string_to_number(month_str):
+    months = { 'Jan' : 1, 'Feb' : 2, 'Mar' : 3, 'Apr' : 4,
+               'May' : 5, 'Jun' : 6, 'Jul' : 7, 'Aug' : 8,
+               'Sep' : 9, 'Oct' : 10, 'Nov' : 11, 'Dec' : 12 }
+    return months[month_str]
+
+def date_format_1(row, year):
+    date_string = row[0].get_text()
+    regex = r"(\d{1,2}).(\d{1,2})\s(\w{3})"
+    search_results = re.search(regex, date_string)
+    if (search_results):
+        start_day_str = search_results.group(1)
+        end_day_str = search_results.group(2)
+        month_str = search_results.group(3)
+
+        month = month_string_to_number(month_str)
+
+        start_date = dt.date(year, month, int(start_day_str))
+        end_date = dt.date(year, month, int(end_day_str))
+
+        dates = { 'start_date' : start_date, 'end_date' : end_date }
+        return dates
+    else:
+        return None
+
+def date_format_3(row, year):
+    date_string = row[0].get_text()
+    regex = r"(\d{1,2})\s(\w{3})"
+    search_results = re.search(regex, date_string)
+    if (search_results):
+        start_day_str = search_results.group(1)
+        month_str = search_results.group(2)
+        month = month_string_to_number(month_str)
+
+        start_date = dt.date(year, month, int(start_day_str))
+
+        dates = { 'start_date' : start_date, 'end_date' : start_date }
+        return dates
+    else:
+        return None
+
 # parse_poll_date
 # parse the poll date from the poll row
-def parse_poll_date(row):
-    return { 'start_date' : "",
-             'end_date' : "" }
+def parse_poll_date(row, year):
+    # date_format_1(row, year)
+
+    # if
+    return date_format_1(row, year) or date_format_3(row, year) or { 'start_date' : "", 'end_date' : "" }
 
 # parse_pollster_client_string
 # get the pollster and the client from the polllster/client string
 def parse_pollster_client_string(row):
     pollster_client_string = row[1].get_text()
-    print(pollster_client_string)
     groups = re.search(r"^(\w+)\/{0,1}(.*)", pollster_client_string)
     return { 'pollster' : groups.group(1),
              'client' : groups.group(2) }
@@ -86,10 +129,10 @@ def parse_scores(row):
              'other' : parse_score_number(row[9])}
 
 # parse_poll_row
-def parse_poll_row(row):
+def parse_poll_row(row, year):
     scores = parse_scores(row)
-    return { 'start_date' : parse_poll_date(row)['start_date'],
-             'end_date' : parse_poll_date(row)['end_date'],
+    return { 'start_date' : parse_poll_date(row, year)['start_date'],
+             'end_date' : parse_poll_date(row, year)['end_date'],
              'pollster' : parse_pollster(row),
              'client' : parse_client(row),
              'sample' : parse_sample(row),
@@ -102,7 +145,7 @@ def parse_poll_row(row):
              'other' :  scores['other']}
 
 # Get current year
-this_year = dt.now().year
+this_year = dt.datetime.now().year
 soup = get_soup(polls_url)
 tables = ge_poll_tables()
 
@@ -111,11 +154,13 @@ tables = ge_poll_tables()
 print(this_year)
 
 # Loop through each poll results table
+table_counter = 0
 for tab in tables:
+    year = this_year - table_counter
     rows = tab.find_all("tr")
     for row in rows:
         if is_poll_row(row):
             row_elems = row.find_all('td')
-            print(parse_poll_row(row_elems))
+            print(parse_poll_row(row_elems, year))
             print()
-
+    table_counter += 1
